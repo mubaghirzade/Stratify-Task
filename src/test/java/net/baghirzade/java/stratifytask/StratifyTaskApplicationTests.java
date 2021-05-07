bb5ebab39e09a0d8ad0902bd76c8434f33cd91f7
Mushfig Baghirzade <mu.baghirzade@gmail.com>
2021-05-07 20:41:19 +0400

    Initializing Maven project

---
+++ src/test/java/net/baghirzade/java/stratifytask/StratifyTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package net.baghirzade.java.stratifytask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StratifyTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
